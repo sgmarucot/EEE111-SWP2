71c815a9bffebf3a87d6daf6c60c5e1e8a70b43d
sgmarucot <155838215+sgmarucot@users.noreply.github.com>
2024-01-11 21:22:28 +0800

    Add files via upload

---
+++ code/LibDBEntry.py
@@ -0,0 +1,12 @@
+class LibDbEntries:
+    def __init__(self,
+                 id=1,
+                 Title='Title',
+                 Author='Name',
+                 Genre='Genre',
+                 status='status'):
+        self.id = id
+        self.Title = Title
+        self.Author = Author
+        self.Genre = Genre
+        self.status = status
